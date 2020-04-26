147f37bdecfa3af920fffaab8e45744c3845a8f6
Emma Prené <emma.prene@gmail.com>
2020-04-26 16:55:48 +0200

    added products data

--- code/src/components/Product.js
+++ code/src/components/Product.js
@@ -12,8 +12,11 @@ export const Product = () => {
     <>
       {scanned.product && scanned.status == 1 && (
         <div>
-          <h1>This is your product</h1>
+
+          <h1>This is cheese {scanned.product.nutriscore_data.is_cheese}</h1>
           <h2>{scanned.product.product_name}</h2>
+          <h2>Brand: {scanned.product.brands}</h2>
+          <img src={scanned.product.image_url} />
         </div>
       )}
       {scanned.status === 0 && <h1 className="not-found">{scanned.status_verbose}</h1>}
